458243545c17e1b2dfa17265d5c1bacc7363f483
RohitR311 <rohit.rajan031101@gmail.com>
2024-11-18 22:17:24 +0530

    feat: add robot duplicate modal

--- src/components/molecules/RobotDuplicate.tsx
+++ src/components/molecules/RobotDuplicate.tsx
@@ -97,6 +97,8 @@ export const RobotDuplicationModal = ({ isOpen, handleStart, handleClose, initia
             return;
         }
     
+        console.log("handle save");
+
         try {
             const success = await duplicateRecording(robot.recording_meta.id, targetUrl);
     
