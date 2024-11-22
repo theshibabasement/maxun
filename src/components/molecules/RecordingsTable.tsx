426d02d649d0d142bfc7692f710ebfbdf3dbf093
amhsirak <karishmashuklaa@gmail.com>
2024-11-22 21:56:46 +0530

    chore: remove unused imports

--- src/components/molecules/RecordingsTable.tsx
+++ src/components/molecules/RecordingsTable.tsx
@@ -9,24 +9,15 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { useEffect } from "react";
 import { WorkflowFile } from "maxun-core";
-
-
 import SearchIcon from '@mui/icons-material/Search';
 import { IconButton, Button, Box, Typography, TextField, MenuItem, Menu, ListItemIcon, ListItemText } from "@mui/material";
 import { Schedule, DeleteForever, Edit, PlayCircle, Settings, Power, ContentCopy, MoreHoriz } from "@mui/icons-material";
-
-import LinkIcon from '@mui/icons-material/Link';
 import { useGlobalInfoStore } from "../../context/globalInfo";
 import { checkRunsForRecording, deleteRecordingFromStorage, getStoredRecordings } from "../../api/storage";
 import { Add } from "@mui/icons-material";
 import { useNavigate } from 'react-router-dom';
 import { stopRecording } from "../../api/recording";
 import { GenericModal } from '../atoms/GenericModal';
-
-import axios from 'axios';
-import { apiUrl } from '../../apiConfig';
-import { Menu as MenuIcon } from '@mui/icons-material';
-
 
 /** TODO:
  *  1. allow editing existing robot after persisting browser steps
